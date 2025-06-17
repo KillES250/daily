0cffa7bb85da7ee100a443258637070c8d4475e5
FyjFyf <66484322@qq.com>
2025-06-17 10:03:40 +0800

    Update start.js

--- mudsrc/events/start/start.js
+++ mudsrc/events/start/start.js
@@ -1,10 +1,10 @@
 const path = require('path');
 const logger = require(path.resolve(__dirname, '../../../server/logger'));
 
-module.exports = function start(){
-    logger.success(`「${this.userConfig.name}」登录成功`);
-    this.cmd.send('setting auto_pfm 0;setting auto_pfm2 0;setting auto_work 0;setting auto_get 1;setting no_message 1');
-    this.cmd.send('relive;cr over;lkfb ok')
-    this.cmd.send('stopstate');
-    this.cmd.send(this.userConfig.loginCommand);
+module.exports = function start(data,cmd){
+    logger.success(`「${data.name}」登录成功`);
+    cmd.send('setting auto_pfm 0;setting auto_pfm2 0;setting auto_work 0;setting auto_get 1;setting no_message 1');
+    cmd.send('relive;cr over;lkfb ok')
+    cmd.send('stopstate');
+    cmd.send(data.loginCommand);
 }
